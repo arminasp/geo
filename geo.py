35a7c04ca96ead0dd1bf7c9e8e242441af0f2437
arminasp <arminasp@gmail.com>
2019-07-14 19:32:38 +0300

    Add language support

--- geo.py
+++ geo.py
@@ -1,13 +1,15 @@
 import csv
 from sys import argv
-from requests import get
+
 from geograpy import get_place_context
+from requests import get
 
-if len(argv) < 2:
-    exit('Usage: python %s [inputfile] [outputfile]' % argv[0])
+if len(argv) < 3:
+    exit('Usage: python %s [isolanguage] [inputfile] [outputfile]' % argv[0])
 
-input_file_path = argv[1]
-output_file_path = argv[2] if len(argv) > 2 else 'output.csv'
+language_code = argv[1]
+input_file_path = argv[2]
+output_file_path = argv[3] if len(argv) > 3 else 'output.csv'
 api_url = 'http://92.62.139.201:8080/api/geonames/countries'
 
 print("Opening input file '%s'..." % input_file_path)
@@ -16,22 +18,32 @@ with open(input_file_path, 'r', encoding='utf-8') as file:
     text = file.read().replace('\n', ' ')
 
 chunks = [text[i:i + 100000] for i in range(0, len(text), 100000)]
-results = []
 output = {}
+results = []
 
-print('Searching for geo names...')
+print("Searching for geo names in '%s' language ..." % language_code)
 
 for chunk in chunks:
     places = get_place_context(text=chunk).countries
+    temp_results = {}
 
     for place in places:
         if place[0].isupper() and place not in results and place in chunk:
-            geo_names = get(api_url, {'name': place}).json()
+            geo_names = get(api_url, {
+                'name': place,
+                'isolanguage': language_code
+            }).json()
 
             if len(geo_names) > 0:
-                output[chunk.find(place)] = geo_names
+                pos = chunk.find(place)
+                output[pos] = geo_names
+                temp_results[pos] = place
                 results.append(place)
-                print('\t%s' % place)
+
+    for key in sorted(temp_results.keys()):
+        print('\t%s' % temp_results[key])
+
+    temp_results.clear()
 
 print('Found %d results...' % len(results))
 
